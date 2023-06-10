cbbe304c9ceb6bc6e9f0b6d4bd9947b9c5e22267
Marina Tilton <m.v.stoyan@gmail.com>
2023-06-10 14:45:23 -0700

    readme

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,21 @@
+const Order = require('../models/Order');
+const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { chechPermissions } = require('../utils');
+
+
 const createOrder = async (req, res) => {
+    const { items: cartItems, tax, shippingFee } = req.body;
+
+    if (!cartItems || cartItems.lenght < 1) {
+        throw new CustomError.BadRequestError('No cart items provided');
+    }
+    if (!tax || !shippingFee) {
+        throw new CustomError.BadRequestError(
+            'Please provide tax and shipping fee'
+        );
+    }
     res.send('create order')
 };
 const getAllOrders = async (req, res) => {
