d52aa68225486052f7fd0a8c243d041733f58daa
Marina Tilton <m.v.stoyan@gmail.com>
2023-06-10 17:11:01 -0700

    app.js

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ const app = express();
 // rest of the packages
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const cors = require('cors');
+const { JSDOM } = require('jsdom');
+const createDOMPurify = require('dompurify');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
 
 // database
 const connectDB = require('./db/connect');
@@ -25,9 +35,42 @@ const orderRouter = require('./routes/orderRoutes');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy' , 1);
+app.use(
+  rateLimiter({
+    winwowMs: 15 * 60 *1000,
+    max: 60,
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(mongoSanitize());
+
 app.use(morgan('tiny'))
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use((req, res, next) => {
+  // Clean user input in req.body
+  req.body = sanitizeInput(req.body);
+
+  next();
+});
+
+function sanitizeInput(input) {
+  if (typeof input === 'object') {
+    // Recursively sanitize all properties of the object
+    for (let key in input) {
+      input[key] = sanitizeInput(input[key]);
+    }
+  } else if (typeof input === 'string') {
+    // Sanitize the string from potentially dangerous elements
+    input = DOMPurify.sanitize(input);
+  }
+
+  return input;
+}
 
 app.use(express.static('./public'));
 app.use(fileUpload());
